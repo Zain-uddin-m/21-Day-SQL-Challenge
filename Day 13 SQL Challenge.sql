54c17e3efa72cac39b54a2cae139a02c0b4b777e
Zain-uddin-m <zaynuddin0@outlook.com>
2026-05-11 21:15:45 +0530

    Day 13 sql adjusted

--- Day 13 SQL Challenge.sql
+++ Day 13 SQL Challenge.sql
@@ -1,7 +1,7 @@
--- Day 13 of 21-SQL Challenge
+-- Day 13 of 21-Day SQL Challenge
 -- Topics: INNER JOIN, joining two tables, relationship understanding
 
--- Day 13 SQL question:
+-- Day 13 SQL Challenge question:
 -- Create a comprehensive report showing patient_id, patient name, age, service, and the total number of staff members
 -- available in their service. Only include patients from services that have more than 5 staff members.
 -- Order by number of staff descending, then by patient name.
